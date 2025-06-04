6728c99d9072d359897466b5045863fd277dde91
GrowthToBetter <baratrahjaga@gmail.com>
2025-06-05 01:10:25 +0700

    init 2'

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,23 +1,27 @@
-import { role_user } from "@/generated/prisma";
+import { DefaultSession, DefaultUser } from "next-auth"
+import { JWT as DefaultJWT } from "next-auth/jwt"
+import { role_user } from "@/generated/prisma"
 
+// 1. Extend the User object returned in the session
 declare module "next-auth" {
-  interface User {
-    role: role_user;
-    id: string;
+  interface User extends DefaultUser {
+    id: string
+    role: role_user
   }
 
   interface Session {
-    user: User & {
-      id: string;
-      role: role_user;
-    } & DefaultSession["user"];
+    user: {
+      id: string
+      role: role_user
+    } & DefaultSession["user"]
   }
 }
 
+// 2. Extend the JWT type used in callbacks
 declare module "next-auth/jwt" {
-  interface JWT {
-    role: role_user;
-    id: string;
-    tokenUpdatedAt: number;
+  interface JWT extends DefaultJWT {
+    id: string
+    role: role_user
+    tokenUpdatedAt?: number
   }
 }
