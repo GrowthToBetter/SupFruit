0401ef82a7fe778a73fe3206cf1ff9c3446048db
GrowthToBetter <baratrahjaga@gmail.com>
2025-06-05 00:33:59 +0700

    .

--- src/env.js
+++ src/env.js
@@ -14,7 +14,7 @@ export const env = createEnv({
     DATABASE_URL: z.string().url(),
     NODE_ENV: z
       .enum(["development", "test", "production"])
-      .default("development"),
+      .default("development"), 
     CLOUDINARY_URL: z.string().url(),
     GOOGLE_CLIENT_SECRET: z.string(),
   },
