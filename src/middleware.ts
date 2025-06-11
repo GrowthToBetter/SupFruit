6e3979f24549762d5e576199a2e3fc313fb5dac0
GrowthToBetter <baratrahjaga@gmail.com>
2025-06-11 08:31:24 +0700

    middleware

---
+++ src/middleware.ts
@@ -0,0 +1,31 @@
+import NextAuth from "next-auth";
+import { authConfig } from "./lib/config/auth.cofig";
+import { NextResponse } from "next/server";
+
+const { auth } = NextAuth(authConfig);
+export default auth(async function middleware(req) {
+  const isLoggedIn = !!req.auth;
+  const isOnAuthPage = req.nextUrl.pathname.startsWith("/auth");
+  const isOnAdminPage = req.nextUrl.pathname.startsWith("/admin");
+
+  if (!isLoggedIn && !isOnAuthPage) {
+    return NextResponse.redirect(new URL("/auth/signin", req.nextUrl));
+  }
+
+  if (isLoggedIn && isOnAuthPage) {
+    return NextResponse.redirect(
+      new URL(
+        req.auth?.user?.role !== "Admin" ? "/dashboard" : "/admin",
+        req.nextUrl
+      )
+    );
+  }
+
+  if (isOnAdminPage && req.auth?.user?.role !== "Admin") {
+    return NextResponse.redirect(new URL("/dashboard", req.nextUrl));
+  }
+});
+
+export const config = {
+  matcher: ["/dashboard/:path*", "/admin/:path*", "/auth/signin"],
+};
